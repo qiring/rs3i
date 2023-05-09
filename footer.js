56ecf198e545a666200955a0975250500d502ebf
qiring <132793160+qiring@users.noreply.github.com>
2023-05-09 11:45:28 +0900

    index

---
+++ footer.js
@@ -0,0 +1,1 @@
+document.write('<footer><div class="d1" id="footer">//</footer>');
---
+++ header.js
@@ -0,0 +1,1 @@
+document.write('<header><div class="d1" id="header"><a href="http://qiring.net/">qiring.net</a></div></header>');
