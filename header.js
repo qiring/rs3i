62c74c07e73e0296af2fe9ebea134311328be255
qiring <132793160+qiring@users.noreply.github.com>
2023-05-09 11:48:36 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-document.write('<header><div class="d1" id="header"><a href="http://qiring.net/">qiring.net</a></div></header>');
+document.write('<header><div class="d1" id="header"><a href="https://qiring.net/">qiring.net</a></div></header>');
