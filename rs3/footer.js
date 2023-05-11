a85f7ed1bc5ca8d82b068c8769ab4b633baed5de
qiring <132793160+qiring@users.noreply.github.com>
2023-05-11 11:17:08 +0900

    remove "www" from canonical

--- rs3/footer.js
+++ rs3/footer.js
@@ -1,1 +1,1 @@
-document.write('<footer><div class="d1" id="footer"><a href="http://qiring.net/rs3/index.html">トップページ</a></div></footer>');
+document.write('<footer><div class="d1" id="footer"><a href="http://qiring.net/rs3/">トップページ</a></div></footer>');
--- rs3/header.js
+++ rs3/header.js
@@ -1,1 +1,1 @@
-document.write('<header><div class="d1" id="header"><a href="http://qiring.net/rs3/index.html">ロマサガ3のアイテムのページ</a></div></header>');
+document.write('<header><div class="d1" id="header"><a href="http://qiring.net/rs3/">ロマサガ3のアイテムのページ</a></div></header>');
